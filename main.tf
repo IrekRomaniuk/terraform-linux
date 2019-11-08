308de1f9f1aeee4d6ddde201d0154b8ddc094ea2
Irek Romaniuk <irekromaniuk@Ireks-Mac-mini.local>
2019-11-08 12:01:09 -0500

    updated data disk

--- main.tf
+++ main.tf
@@ -29,6 +29,7 @@ resource "azurerm_virtual_machine" "vm" {
     caching           = "ReadWrite"
     create_option     = "Empty"
     disk_size_gb      = "${var.data_size}"
+    lun = 0
     #managed_disk_type = "StandardSSD_LRS"
     vhd_uri       = "${data.azurerm_storage_account.store.primary_blob_endpoint}${azurerm_storage_container.container.name}/${var.prefix}datadisk.vhd"
   }
